82748f67795f5d26a8070b33842fb0eaeb7495cd
carlos <lab8455@caelum126-17>
2019-10-12 12:08:49 -0300

    exercicio 2.6

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.form.SalaForm;
 
@@ -14,16 +15,15 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.validation.Valid;
 import java.util.Optional;
 
-/**
- * Created by nando on 03/03/17.
- */
 @Controller
 public class SalaController {
 
     @Autowired
     private SalaDao salaDao;
 
-
+    @Autowired
+    private SessaoDao sessaoDao;
+    
     @GetMapping({"/admin/sala", "/admin/sala/{id}"})
     public ModelAndView form(@PathVariable("id") Optional<Integer> id, SalaForm salaForm) {
         ModelAndView modelAndView = new ModelAndView("sala/sala");
@@ -66,7 +66,8 @@ public class SalaController {
 
         ModelAndView view = new ModelAndView("sessao/lista");
         view.addObject("sala", sala);
-
+        view.addObject("sessoes", sessaoDao.buscaSessoesDaDala(sala));
+        
         return view;
     }
 
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,19 +2,36 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
-public class Sessao {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Entity
+public class Sessao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
+	
+	public Sessao() {
+	}
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
